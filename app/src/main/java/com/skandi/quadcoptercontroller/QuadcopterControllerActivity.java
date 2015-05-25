593c1465c157b26ff1ed4bd98a47eda24c24aec4
Skandi <neloangelos@msn.com>
2015-05-25 16:32:00 +0800

    add BLE

--- app/src/main/java/com/skandi/quadcoptercontroller/QuadcopterControllerActivity.java
+++ app/src/main/java/com/skandi/quadcoptercontroller/QuadcopterControllerActivity.java
@@ -1,8 +1,16 @@
 package com.skandi.quadcoptercontroller;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -12,11 +20,23 @@ public class QuadcopterControllerActivity extends Activity {
     private TextView angleTextView;
     private TextView powerTextView;
     private JoystickView joystick;
+    private final static String TAG = QuadcopterControllerActivity.class.getSimpleName();
+
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private BluetoothLeService mBluetoothLeService;
+    private boolean mConnected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quadcopter_controller);
+
+
+
         angleTextView = (TextView) findViewById(R.id.angleTextView);
         powerTextView = (TextView) findViewById(R.id.powerTextView);
         joystick = (JoystickView) findViewById(R.id.JoystickView);
@@ -45,9 +65,99 @@ public class QuadcopterControllerActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }*/
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+        unbindService(mServiceConnection);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //this.unregisterReceiver(mGattUpdateReceiver);
+        //unbindService(mServiceConnection);
+        if(mBluetoothLeService != null)
+        {
+            mBluetoothLeService.close();
+            mBluetoothLeService = null;
+        }
+        Log.d(TAG, "We are in destroy");
+    }
+
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+
+            Log.e(TAG, "mBluetoothLeService is okay");
+            // Automatically connects to the device upon successful start-up initialization.
+            //mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    // Handles various events fired by the Service.
+    // ACTION_GATT_CONNECTED: connected to a GATT server.
+    // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
+    // ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
+    // ACTION_DATA_AVAILABLE: received data from the device.  This can be a result of read
+    //                        or notification operations.
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {  //连接成功
+                Log.e(TAG, "Only gatt, just wait");
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) { //断开连接
+                mConnected = false;
+                invalidateOptionsMenu();
+            }else if(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) //可以开始干活了
+            {
+                mConnected = true;
+                Log.e(TAG, "In what we need");
+                invalidateOptionsMenu();
+            }else if (BluetoothLeService.ACTION_DATA_AVAILABLE.equals(action)) { //收到数据
+                Log.e(TAG, "RECV DATA");
+                String data = intent.getStringExtra(BluetoothLeService.EXTRA_DATA);
+            }
+        }
+    };
+    private static IntentFilter makeGattUpdateIntentFilter() {                        //注册接收的事件
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(BluetoothDevice.ACTION_UUID);
+        return intentFilter;
+    }
+
 }
